3d65f21564cb621cf6ec01be7b2a9c999d83ca81
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-11 11:46:10 -0700

    route index file changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('../routes/apiRoutes');
-const htmlRoutes = require('../routes/htmlRoutes');
+const apiRoutes = require('./apiRoutes');
+const htmlRoutes = require('./htmlRoutes');
 
 router.use(apiRoutes);
 router.use(htmlRoutes);
